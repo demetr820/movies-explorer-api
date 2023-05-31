7d6b03c9824ae18a6417e71cbfda4d311b9582a5
Dmitry Gordeev <dima.1418@mail.ru>
2023-05-31 13:36:00 +0300

    commit 1

---
+++ utils/validation.js
@@ -0,0 +1,62 @@
+const { celebrate, Joi } = require("celebrate");
+const { validateURL } = require("./consts");
+// const BadRequestError = require("../errors/BadRequestError");
+
+const validation = {
+  validateUserParams: celebrate({
+    params: Joi.object().keys({
+      userID: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  validateMovieParams: celebrate({
+    params: Joi.object().keys({
+      ID: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  validateMovie: celebrate({
+    body: Joi.object().keys({
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required(),
+      trailerLink: Joi.string().required(),
+      // owner: Joi.string().length(24).hex().required(),
+      nameRU: Joi.string()
+        .required()
+        .regex(/^[а-яА-Я0-9\s]+$/),
+      nameEN: Joi.string()
+        .required()
+        .regex(/^[a-zA-Z0-9\s]+$/),
+      thumbnail: Joi.string().required(),
+      movieId: Joi.number().required(),
+    }),
+  }),
+  validateProfile: celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      email: Joi.string().min(2).max(30),
+    }),
+  }),
+  // validateAvatar: celebrate({
+  //   body: Joi.object().keys({
+  //     avatar: Joi.string().custom(validateURL).required(),
+  //   }),
+  // }),
+  validateSignup: celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  validateSignin: celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+};
+
+module.exports = validation;
